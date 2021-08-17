1f47823ff30d3a0ff3e920c50fa32d9c85ebd2b3
18awebster <67443160+aliciawebster@users.noreply.github.com>
2021-08-17 02:04:13 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let questions = [
+  ['kowhai', 'yellow flowers in spring, small leaflets, can grow up to 25m', 'kowhai.jpg'],
+  ['kahikatea', 'conifer (cone bearing) tree, can grow over 50m', 'kahikatea.jpg'],
+  ['poroporo', 'fleshy shrub, yellow/orange fruits, purple ruffled flowers, up to 4m', 'poroporo.jpg'],
+  ['mingimingi', 'divaricating shrub, small oblong leaves, translucent blue berries', 'mingimingi.jpg'],
+  ['karamu', 'large bushy shrub, red fruits in tight clusters along twigs, grows to 4m', 'karamu.jpg'],
+  ['lancewood', 'thin straight trunk, lance-like foliage, drooping spiky leaves, up to 15m', 'lancewood.jpg'],
+  ['akeake', 'bushy shrub/small tree, reddish bark, long thin wavy leaves, greenish white flowers with yellow or red tips, up to 12m', 'akeake.jpg'],
+  ['cabbage tree', 'long narrow leaves up to 1m, white flowers turn to bluish-white berries, height up to 20m', 'cabbagetree.jpg'],
+  ['carex secta', 'tussock forming sedge, 1.5-7mm wide light green/yellow-green leaves, up to 1.5m', 'carexsecta.jpg'],
+  ['mahoe', 'shrub or small tree, greenish flowers in clusters along twigs, purple fruit, up to 15m', 'mahoe.jpg']
+  ]
+
+</script>
+
